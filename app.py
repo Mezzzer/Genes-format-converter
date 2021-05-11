0d05f0e5cb5f483ac865d3d4e1de597832f431c3
inf136768 <maciej.meler@student.put.poznan.pl>
2021-05-11 20:25:06 +0200

    error handling, text fields and names of duplicates

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from processing_functions import *
+from jupyter_dash import JupyterDash
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -8,8 +9,7 @@ from dash.dependencies import Input, Output, State
 import dash_table
 import dash_bootstrap_components as dbc
 
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets=[dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -26,119 +26,244 @@ app.layout = html.Div(children=[
             'background': '#1abc9c',
             'color': 'white',
             'font-size': '30px'}),
-
+        
     html.Hr(),
     
     html.Center(children=[
-        html.Label('Upload list of genes in HGNC format for analysis (txt files only):'),
-        dcc.Upload(
-            id='upload-data',
-            children=html.Div([
-                 'Drag and Drop or ',
-                html.A('Select File')
+        dbc.Row(
+            [              
+                dbc.Col(children=[
+                    html.Label('Provide list of genes in HGNC format for analysis...'),
+                    html.Br(),
+                    dcc.Textarea(
+                        id='insert-textarea',
+                        value='',
+                        style={'width': '80%', 'height': 200},
+                        disabled=False, draggable='false'
+                    )
+
+                ]),
+                dbc.Col(children=[
+                    html.Label('...or upload it here (txt files only):'),
+                    dcc.Upload(
+                        id='upload-data',
+                        children=html.Div([
+                             'Drag and Drop or ',
+                            html.A('Select File')
+                        ]),
+                        style={
+                            'width': '50%',
+                            'height': '60px',
+                            'lineHeight': '60px',
+                            'borderWidth': '1px',
+                            'borderStyle': 'dashed',
+                            'borderRadius': '5px',
+                            'textAlign': 'center',
+                            'margin': '10px'
+                        }, multiple=False
+                    )
+                ])
             ]),
-            style={
-                'width': '25%',
-                'height': '60px',
-                'lineHeight': '60px',
-                'borderWidth': '1px',
-                'borderStyle': 'dashed',
-                'borderRadius': '5px',
-                'textAlign': 'center',
-                'margin': '10px'
-            }, multiple=False),
-        html.Div([html.P(id = "upload-status",
-                     children=["File not uploaded"], 
-                     style={'font-style': 'italic'})]),
+        
     ]),
 
     html.Hr(),
     
     dcc.Tabs([
         dcc.Tab(label='Convert to Ensembl Gene IDs', children=[
-            html.Br(),
-            dcc.Textarea(
-                id='converted-textarea',
-                value='',
-                style={'width': '50%', 'height': 200},
-                disabled=True, draggable='false'
-            ),
-            html.Br(),
-            html.Button('Convert', id='convert-button', n_clicks=0),
+            html.Center(children=[
+                html.Br(),
+                dcc.Textarea(
+                    id='converted-textarea',
+                    value='',
+                    style={'width': '50%', 'height': 200},
+                    disabled=True, draggable='false'
+                ),
+                html.Br(),
+                dbc.Button('Convert', color="success", id='convert-button', n_clicks=0, outline=True),
+                ])
         ]),
         dcc.Tab(label='Check for repeated genes', children=[
-             html.Br(),
-             html.Label('Upload additional files: '),
-             dcc.Upload(
-                id='upload-add-data',
-                children=html.Div([
-                    'Drag and Drop or ',
-                    html.A('Select Files')
-                ]),
-                style={
-                    'width': '30%',
-                    'height': '60px',
-                    'lineHeight': '60px',
-                    'borderWidth': '1px',
-                    'borderStyle': 'dashed',
-                    'borderRadius': '5px',
-                    'textAlign': 'center',
-                    'margin': '10px'
-                },multiple=True),
-            html.Div([html.P(id = "upload-add-status",
-                     children=["File not uploaded"], 
-                     style={'font-style': 'italic'})]),
             html.Br(),
-            html.Label('List of genes which appear in additional files: '),
-            dcc.Textarea(
-                id='repeated-genes-textarea',
-                value='',
-                style={'width': '50%', 'height': 200},
-                disabled=True, draggable='false'),
-            html.Br(),
-            html.Button('Check', id='check-button', n_clicks=0)
-        ])
-    ])
-])
+            html.Center(children=[
+            dbc.Row([              
+                    dbc.Col(children=[
+                        html.Label('Provide additional list...'),
+                        html.Br(),
+                        dcc.Textarea(
+                            id='insert-add-textarea',
+                            value='',
+                            style={'width': '80%', 'height': 200},
+                            disabled=False, draggable='false'
+                        )
 
-@app.callback(Output('upload-status', 'children'),
-              Input('upload-data', 'contents'),
-              State('upload-data', 'filename'))
-def update_output(contents, filename):
+                    ]),
+                    dbc.Col(children=[
+                        html.Label('...or upload it here:'),
+                        dcc.Upload(
+                            id='upload-add-data',
+                            children=html.Div([
+                                 'Drag and Drop or ',
+                                html.A('Select Files')
+                            ]),
+                            style={
+                                'width': '50%',
+                                'height': '60px',
+                                'lineHeight': '60px',
+                                'borderWidth': '1px',
+                                'borderStyle': 'dashed',
+                                'borderRadius': '5px',
+                                'textAlign': 'center',
+                                'margin': '10px'
+                            }, multiple=True
+                        )
+                ])
+            ]),
+        ]),
+        html.Br(),
+        html.Center(children=[
+            html.Label('List of genes which appear in additional files: '),
+            html.Br(),
+            dbc.Row([
+                dbc.Col(children=[
+                    html.Label('Ensembl IDs'),
+                    html.Br(),
+                    dcc.Textarea(
+                        id='repeated-genes-textarea',
+                        value='',
+                        style={'width': '80%', 'height': 200},
+                        disabled=True, draggable='false')
+                        ]),
+                dbc.Col(children=[
+                    html.Label('HGNC name (GRCh37)'),
+                    html.Br(),
+                     dcc.Textarea(
+                        id='repeated-names-textarea',
+                        value='',
+                        style={'width': '80%', 'height': 200},
+                        disabled=True, draggable='false')
+                ])
+            ]),
+            html.Br(),
+            dbc.Button('Check', color="success", id='check-button', n_clicks=0, outline=True)
+            ])
+        ])
+    ],colors={
+        "border": "white",
+        "primary": "#1abc9c",
+        "background": "#E0E0E0"}
+    ),
+    
+    html.Br(),
+
+    html.Footer(children=[
+        html.H5(children='for MNM Diagnostics'),
+        html.Div(children='''
+            by Maciej Meler
+            ''', style={'font-style': 'italic'})
+        ],        
+        style={
+            'padding': '10px',
+            'text-align': 'right',
+            'background': '#1abc9c',
+            'color': 'white',
+            'font-size': '12px'}),
+
+    #erro dialogs        
+    dcc.ConfirmDialog(
+        id='only-txt',
+        message='You can upload only txt file!',
+    ),
+    dcc.ConfirmDialog(
+        id='only-txts',
+        message='You can upload only txt files!',
+    ),
+    dcc.ConfirmDialog(
+        id='not-valid-gene',
+        message='You have to provide a valid genes list of genes. \nCheck if all names are in GRCh37 or GRCh38 format and seperated in new lines.',
+    ),
+    dcc.ConfirmDialog(
+        id='not-valid-genes',
+        message='You have to provide a valid genes list of genes. \nCheck if all names are in GRCh37 or GRCh38 format and seperated in new lines.',
+    ),
+])
+    
+@app.callback(
+    [
+        Output('insert-textarea', 'value'),
+        Output('only-txt', 'displayed')
+    ],
+    [
+        Input('upload-data', 'contents'),
+        State('upload-data', 'filename')
+    ])
+def upload_file(contents, filename):
     if contents is not None:
-        return upload_file_prompt(filename)
+        if '.txt' in filename:
+            return "\n".join(parse_file(contents)), False
+        else:
+            return None, True
+    return None, False
     
-@app.callback(Output('upload-add-status', 'children'),
-              Input('upload-add-data', 'contents'),
-              State('upload-add-data', 'filename'))
-def add_update_output(list_of_contents, list_of_filenames):
+@app.callback(
+    [
+        Output('converted-textarea', 'value'),
+        Output('not-valid-gene', 'displayed')
+    ],
+    [
+        Input('convert-button', 'n_clicks'),
+        State('insert-textarea', 'value')
+    ])
+def convert_file(n_clicks, value):
+    if value is not None:
+        try:
+            return "\n".join(process_list(value.split())), False
+        except:
+            return None, True
+    return None, False
+
+@app.callback(
+    [
+        Output('insert-add-textarea', 'value'),
+        Output('only-txts', 'displayed')
+    ],
+    [
+        Input('upload-add-data', 'contents'),
+        State('upload-add-data', 'filename')
+    ])
+def additional_upload(list_of_contents, list_of_names):
     if list_of_contents is not None:
-        return upload_add_file_prompt(list_of_filenames)
+        parsed_files = list()
+        for c, f in zip(list_of_contents, list_of_names):
+            if '.txt' in f:
+                parsed_files = parsed_files + parse_file(c)
+            else:
+                return None, True
+        return "\n".join(parsed_files), False
+    return None, False
     
-@app.callback(Output('converted-textarea', 'value'),
-              Input('convert-button', 'n_clicks'),
-              State('upload-data', 'contents'),
-              State('upload-data', 'filename'))
-def convert_file(n_clicks, contents, filename):
-    if contents is not None and '.txt' in filename:
-        return "\n".join(parse_file(contents))
-    
-@app.callback(Output('repeated-genes-textarea', 'value'),
-              Input('check-button', 'n_clicks'),
-              State('upload-add-data', 'contents'),
-              State('upload-add-data', 'filename'),
-              State('upload-data', 'contents'),
-              State('upload-data', 'filename'))
-def additional_upload(n_clicks, list_of_contents, list_of_names, contents, filename):
-    if contents is not None and '.txt' in filename:
-        parsed_file = parse_file(contents)
-        if list_of_contents is not None:
-            parsed_files = list()
-            for c, f in zip(list_of_contents, list_of_names):
-                if '.txt' in filename:
-                    parsed_files.append(parse_file(c))
-        if parsed_files is not None:
-            return "\n".join(find_repeated(parsed_file, parsed_files))
+@app.callback(
+    [
+        Output('repeated-genes-textarea', 'value'),
+        Output('repeated-names-textarea', 'value'),
+        Output('not-valid-genes', 'displayed')
+    ],
+    [
+        Input('check-button', 'n_clicks'),
+        State('insert-textarea', 'value'),
+        State('insert-add-textarea', 'value')
+    ])
+def additional_check(n_clicks, genes1, genes2):
+    if genes1 is not None and genes2 is not None:
+        try:
+            processed_genes1 = process_list(genes1.split())
+            processed_genes2 = process_list(genes2.split())
+            ensembls = find_repeated(processed_genes1, processed_genes2)
+            names = ensembl_list_to_GRCh37(ensembls)
+            return "\n".join(ensembls), "\n".join(names), False
+        except:
+            return None, None, True
+    return None, None, False
 
 app.title = 'Genes format converter'
 app.run_server(port = 8090, debug=False)
--- processing_functions.py
+++ processing_functions.py
@@ -6,6 +6,9 @@ def parse_file(contents):
     content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string).decode('utf-8')
     HGNC_list = decoded.split()
+    return HGNC_list
+
+def process_list(HGNC_list):
     GRCh37_list = HGNC_list_to_GRCh37(HGNC_list)
     ensembl_list = HGNC_list_to_ensembl(GRCh37_list)
     return ensembl_list
@@ -22,17 +25,10 @@ def upload_add_file_prompt(list_of_filenames):
     else:
         return 'Files ' + ", ".join(list_of_filenames) + ' uploaded'
 
-def find_repeated(genes_list, list_of_genes_lists):
-    repeated_genes = list()
-    for glist in list_of_genes_lists:
-        repeated_genes = repeated_genes + [g for g in genes_list if g in glist]
-    unique_genes = list(set(repeated_genes))
-    return unique_genes
-
-ensembl_37 = pyensembl.EnsemblRelease(release=75)
-ensembl_38 = pyensembl.EnsemblRelease(release=100)
-
 def GRCh38_to_GRCh37(name):
+    ensembl_37 = pyensembl.EnsemblRelease(release=75)
+    ensembl_38 = pyensembl.EnsemblRelease(release=100)
+
     try:
         #check if name is in GRCh37 table
         ensembl_37.gene_ids_of_gene_name(name)
@@ -43,7 +39,18 @@ def GRCh38_to_GRCh37(name):
     return name
 
 def HGNC_list_to_GRCh37(genes_list):
+    ensembl_38 = pyensembl.EnsemblRelease(release=100)
     return [GRCh38_to_GRCh37(gname) for gname in genes_list]
 
 def HGNC_list_to_ensembl(genes_list):
+    ensembl_37 = pyensembl.EnsemblRelease(release=75)
     return [ensembl_37.gene_ids_of_gene_name(gname)[0] for gname in genes_list]
+
+def ensembl_list_to_GRCh37(genes_list):
+    ensembl_37 = pyensembl.EnsemblRelease(release=75)
+    return [ensembl_37.gene_name_of_gene_id(gname) for gname in genes_list]
+
+def find_repeated(genes_list, another_list):
+    repeated_genes = [g for g in genes_list if g in another_list]
+    unique_genes = list(set(repeated_genes))
+    return unique_genes
